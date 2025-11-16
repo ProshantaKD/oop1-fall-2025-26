817b97567762772c50ae97a491f17dc959c004a4
ProshantaKD <proshantakd2004@gmail.com>
2025-11-17 04:20:55 +0600

    Added java files

---
+++ lab/TASK2.java
@@ -0,0 +1,9 @@
+public class TASK2 {
+    public static void main(String[] args) {
+        double price = 250;
+        double discount = price * 0.15;
+        double finalPrice = price - discount;
+        System.out.println("Final price after discount:" + finalPrice);
+    }
+
+}
